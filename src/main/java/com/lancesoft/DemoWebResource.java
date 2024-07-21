74dd7ed37af1ae2c6c9ab4912b017df43f336452
RatnalaSaiCharan <RatnalaSaiCharan@192.168.1.54>
2024-07-21 10:29:54 +0530

    committing changes for feature 1129

--- src/main/java/com/lancesoft/DemoWebResource.java
+++ src/main/java/com/lancesoft/DemoWebResource.java
@@ -3,6 +3,7 @@ package com.lancesoft;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +17,8 @@ public class DemoWebResource {
 		return new ResponseEntity<>(" Hyy There! ", HttpStatus.OK);
 	}
 
+	@GetMapping("/getInfo/{name}")
+	public ResponseEntity<?> getInfo(@PathVariable(value = "name") String name ) {
+		return new ResponseEntity<>(" Hyy "+name, HttpStatus.OK);
+	}
 }
